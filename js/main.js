23965c094fe712786d62751dac3c4dca786cd5ca
portloop <rasimmatsko0@gmail.com>
2023-01-24 12:38:40 +0200

    end

--- js/main.js
+++ js/main.js
@@ -6,9 +6,64 @@ for (let anchor of anchors) {
         const blockID = anchor.getAttribute('href');
         document.querySelector('' + blockID).scrollIntoView({
             behavior: 'smooth',
-            block: 'nearest'
+            block: 'end'
         })
     })
 }
 
 document.getElementById('video').play();
+
+let buttons = document.querySelectorAll('button.actionScroll');
+let EMAIL_INPUT = document.querySelector('#email-input');
+let inputEmail = document.querySelector('.contact-section-container > form > input')
+let sumbitButton = document.querySelector('.contact-section-container > form > button')
+for(let button of buttons) {
+    button.addEventListener('click', function(e) {
+        EMAIL_INPUT.scrollIntoView({
+            behavior: 'smooth'
+        })
+        inputEmail.style = 'border: 2px solid #BB17F4'
+    })
+}
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const input = document.querySelector('input');
+
+function isEmailValid(value) {
+ 	return EMAIL_REGEXP.test(value);
+}
+
+sumbitButton.addEventListener('click', function(e) {
+    if(isEmailValid(inputEmail.value)) {
+        inputEmail.style = 'border: 2px solid green'
+    } else {
+        inputEmail.style = 'border: 2px solid red'
+    }
+    setTimeout(() => {
+        inputEmail.style = 'border: 0'
+      }, "2000")
+})
+
+let burgerMenu = document.querySelector('.mobile-burger'),
+    headerMenu = document.querySelector('.header-menu'),
+    close = document.querySelector('#close'),
+    links = document.querySelectorAll('.header-menu--items > a'),
+    loginButton = document.querySelector('.header-menu .login-button');
+
+burgerMenu.addEventListener('click', () => {
+    headerMenu.classList.add('active')
+})
+
+close.addEventListener('click', function() {
+    headerMenu.classList.remove('active')
+})
+
+loginButton.addEventListener('click', function() {
+    headerMenu.classList.remove('active')
+})
+
+for(link of links){
+    link.addEventListener('click', function() {
+        headerMenu.classList.remove('active')
+    })
+}
